9a98f13f643e844fd519b1a59d286327d185e33b
raycarter-khlee <rksrn@naver.com>
2020-07-30 16:28:59 +0900

    Move diary

---
+++ _posts/2020-07-29-firstdiary-diaries.md
@@ -0,0 +1,20 @@
+---
+title: "Fisrt Diary in my Homepage"
+date: 2020-07-29 19:41:10
+categories: diary
+---
+
+## Today's Weather
+ - 	Rainy
+
+## Today's Diary
+At last, I made my homepage. At first, I tried to make a bling bling homepage. But I thought I didn't know when to stop writing diary and running the homepage. So I decieded to make a simple homepage using github.
+although I used github, it is so difficult to create a homepage. I surfed the internet to find how to make a homepage and found them.
+I made a homepage referring the two sites below.
+
+  https://devinlife.com/howto%20github%20pages/blog-config/
+  https://dreamgonfly.github.io/blog/jekyll-remote-theme/
+
+Not to forget, write down the two sites.....
+I made it hard for my part, but it still lacks a lot of features.
+Someday, it will get better. I believe...
--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
         "url": "http://localhost:4000/jekyll/update/first-post/"
+      },{
+        "title": "Fisrt Diary in my Homepage",
+        "excerpt":"Today’s Weather RainyToday’s Diary At last, I made my homepage. At first, I tried to make a bling bling homepage. But I thought I didn’t know when to stop writing diary and running the homepage. So I decieded to make a simple homepage using github.although I used github, it is...","categories": ["diary"],
+        "tags": [],
+        "url": "http://localhost:4000/diary/firstdiary-diaries/"
       },]
