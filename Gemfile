227b3b6c0910110cf77317fe9341c17a3547dc35
raycarter-khlee <rksrn@naver.com>
2020-07-31 09:28:11 +0900

    Add Gemfile

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem 'jekyll-sitemap'
+gem 'jekyll-seo-tag'
+gem 'jekyll-feed'
+gem 'jekyll-paginate'
+gem 'jekyll-redirect-from'
--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Today’s Weather RainyToday’s Diary At last, I made my homepage. At first, I tried to make a bling bling homepage. But I thought I didn’t know when to stop writing diary and running the homepage. So I decieded to make a simple homepage using github.although I used github, it is...","categories": ["diary"],
         "tags": [],
         "url": "http://localhost:4000/diary/firstdiary-diaries/"
+      },{
+        "title": "Jekyll 사용해서 GitHub 블로그 만들기 1탄!!",
+        "excerpt":"시작에 앞서…. 블로그를 시작하면서, Directions   Preheat the oven to 350 F.  In a medium bowl, whisk flour with baking soda, nutmeg and salt.  In a","categories": ["Blog","Jekyll","GitHub"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/jekyll/github/second-post/"
       },]
